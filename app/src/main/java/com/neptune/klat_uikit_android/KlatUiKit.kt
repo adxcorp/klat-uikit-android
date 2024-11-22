2e9217df19d3232b524f4706263f1262bb5f0d45
ggone212 <stopone3119@neptunez.co.kr>
2024-11-22 16:39:18 +0900

    [feature] uikit 채널 리스트 화면 플로우 구현

--- app/src/main/java/com/neptune/klat_uikit_android/KlatUiKit.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/KlatUiKit.kt
@@ -1,12 +1,24 @@
 package com.neptune.klat_uikit_android
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import com.neptune.klat_uikit_android.feature.channel.list.ChannelListActivity
+import com.neptune.klat_uikit_android.model.LoginRequest
+import io.talkplus.TalkPlus
 
 object KlatUiKit {
-    fun start(activity: Activity) {
-        val intent = Intent(activity, ChannelListActivity::class.java)
+    fun initialize(context: Context, appId: String) {
+        TalkPlus.init(context, appId)
+    }
+
+    fun connect(activity: Activity, loginRequest: LoginRequest) {
+        val intent = Intent(activity, ChannelListActivity::class.java).apply {
+            putExtra(ChannelListActivity.EXTRA_USER_TOKEN, loginRequest.userToken)
+            putExtra(ChannelListActivity.EXTRA_USER_ID, loginRequest.userId)
+            putExtra(ChannelListActivity.EXTRA_USER_NICKNAME, loginRequest.userNickname)
+            putExtra(ChannelListActivity.EXTRA_USER_PROFILE_IMAGE, loginRequest.profileImage)
+        }
         activity.startActivity(intent)
     }
 }
--- app/src/main/java/com/neptune/klat_uikit_android/feature/channel/list/ChannelListActivity.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/feature/channel/list/ChannelListActivity.kt
@@ -1,12 +1,53 @@
 package com.neptune.klat_uikit_android.feature.channel.list
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.neptune.klat_uikit_android.R
+import com.neptune.klat_uikit_android.databinding.ActivityChannelListBinding
 
 class ChannelListActivity : AppCompatActivity() {
+    private val binding: ActivityChannelListBinding by lazy { ActivityChannelListBinding.inflate(layoutInflater) }
+
+    companion object {
+        const val EXTRA_USER_TOKEN = "extra_user_token"
+        const val EXTRA_USER_ID = "extra_user_ud"
+        const val EXTRA_USER_NICKNAME = "extra_user_nickname"
+        const val EXTRA_USER_PROFILE_IMAGE = "extra_user_profile_image"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_channel_list)
+        setContentView(binding.root)
+        setUi()
+    }
+
+    private fun setUi() = with(binding) {
+        layoutHeader.ivLeftBtn.apply {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_24_back)
+            setOnClickListener { finish() }
+        }
+
+        layoutHeader.tvLeftText.apply {
+            visibility = View.VISIBLE
+            text = "채널"
+        }
+
+        layoutHeader.ivFirstRightBtn.apply {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_24_search)
+            setOnClickListener {
+                // TODO 검색
+            }
+        }
+
+        layoutHeader.ivSecondRightBtn.apply {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_24_add_channel)
+            setOnClickListener {
+                // TODO 채널 추가
+            }
+        }
     }
 }

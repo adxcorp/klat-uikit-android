31d593d54d0ac8c537df3e142c9b2ea8abcb1cbf
ggone212 <stopone3119@neptunez.co.kr>
2024-12-02 15:04:10 +0900

    [feature] 키보드 유틸 확장함수 구현

---
+++ app/src/main/java/com/neptune/klat_uikit_android/core/util/KeyboardExtension.kt
@@ -0,0 +1,16 @@
+package com.neptune.klat_uikit_android.core.util
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+fun Activity.showKeyboard(editText: EditText) {
+    val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun Activity.hideKeyboard(editText: EditText) {
+    val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(editText.windowToken, 0)
+}

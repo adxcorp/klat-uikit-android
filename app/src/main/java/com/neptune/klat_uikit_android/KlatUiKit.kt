729b573c31b5ea91d3c7839be84c53bdc35c5c76
ggone212 <stopone3119@neptunez.co.kr>
2024-12-02 14:40:44 +0900

    [feaure] 로그인 구현

--- app/src/main/java/com/neptune/klat_uikit_android/KlatUiKit.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/KlatUiKit.kt
@@ -3,24 +3,37 @@ package com.neptune.klat_uikit_android
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import com.neptune.klat_uikit_android.feature.channel.list.ChannelListActivity
+import android.widget.Toast
+import com.neptune.klat_uikit_android.feature.channel.main.ChannelActivity
 import com.neptune.klat_uikit_android.model.LoginRequest
 import io.talkplus.TalkPlus
+import io.talkplus.entity.user.TPUser
+import io.talkplus.params.TPLoginParams
+import java.lang.Exception
 
 object KlatUiKit {
     fun initialize(context: Context, appId: String) {
         TalkPlus.init(context, appId)
     }
 
-    // test
-
     fun connect(activity: Activity, loginRequest: LoginRequest) {
-        val intent = Intent(activity, ChannelListActivity::class.java).apply {
-            putExtra(ChannelListActivity.EXTRA_USER_TOKEN, loginRequest.userToken)
-            putExtra(ChannelListActivity.EXTRA_USER_ID, loginRequest.userId)
-            putExtra(ChannelListActivity.EXTRA_USER_NICKNAME, loginRequest.userNickname)
-            putExtra(ChannelListActivity.EXTRA_USER_PROFILE_IMAGE, loginRequest.profileImage)
-        }
-        activity.startActivity(intent)
+        val loginType = loginRequest.userToken?.let { TPLoginParams.LoginType.TOKEN } ?: TPLoginParams.LoginType.ANONYMOUS
+
+        val tpLoginParams = TPLoginParams.Builder(loginRequest.userId, loginType).apply {
+            if (loginType == TPLoginParams.LoginType.TOKEN) { setLoginToken(loginRequest.userToken) }
+            setUserName(loginRequest.userNickname)
+            setProfileImageUrl(loginRequest.profileImage)
+        }.build()
+
+        TalkPlus.login(tpLoginParams, object : TalkPlus.CallbackListener<TPUser> {
+            override fun onSuccess(t: TPUser?) {
+                val intent = Intent(activity, ChannelActivity::class.java)
+                activity.startActivity(intent)
+            }
+
+            override fun onFailure(errorCode: Int, exception: Exception) {
+                Toast.makeText(activity, exception.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }

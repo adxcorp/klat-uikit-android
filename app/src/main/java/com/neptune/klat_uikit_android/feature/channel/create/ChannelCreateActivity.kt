1df39971f3f6d7ace5d8253990c0c75f336b2b0b
ggone212 <stopone3119@neptunez.co.kr>
2024-12-10 13:33:51 +0900

    [feature] 카메라 권한 플로우 구현 및 사진 매핑

--- app/src/main/java/com/neptune/klat_uikit_android/feature/channel/create/ChannelCreateActivity.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/feature/channel/create/ChannelCreateActivity.kt
@@ -1,12 +1,14 @@
 package com.neptune.klat_uikit_android.feature.channel.create
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.neptune.klat_uikit_android.R
+import com.neptune.klat_uikit_android.core.extension.loadThumbnail
 import com.neptune.klat_uikit_android.databinding.ActivityChannelCreateBinding
 
-class ChannelCreateActivity : AppCompatActivity() {
+class ChannelCreateActivity : AppCompatActivity(), PhotoActionListener {
+    private val viewModel: ChannelCreateViewModel by viewModels()
     private val binding: ActivityChannelCreateBinding by lazy { ActivityChannelCreateBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,9 +32,15 @@ class ChannelCreateActivity : AppCompatActivity() {
     }
 
     private fun showPhotoOptionsBottomSheet() {
-        val bottomSheet = PhotoOptionsBottomSheet().apply {
-
-        }
+        val bottomSheet = PhotoOptionsBottomSheet(this)
         bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+    }
+
+    override fun onPhotoCaptured(fileUri: Uri) {
+        binding.ivCreateChannelLogo.loadThumbnail(fileUri)
+    }
+
+    override fun onPhotoSelected(fileUri: Uri) {
+
     }
 }
--- app/src/main/java/com/neptune/klat_uikit_android/feature/channel/create/PhotoOptionsBottomSheet.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/feature/channel/create/PhotoOptionsBottomSheet.kt
@@ -1,16 +1,51 @@
 package com.neptune.klat_uikit_android.feature.channel.create
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.fragment.app.FragmentActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.neptune.klat_uikit_android.databinding.LayoutPhotoOptionsBottomSheetBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class PhotoOptionsBottomSheet : BottomSheetDialogFragment() {
+class PhotoOptionsBottomSheet(private val photoActionListener: PhotoActionListener) : BottomSheetDialogFragment() {
+    private val parentActivity: FragmentActivity by lazy { requireActivity() }
     private var _binding: LayoutPhotoOptionsBottomSheetBinding? = null
     private val binding get() = _binding ?: error("LayoutPhotoOptionsBottomSheetBinding 초기화 에러")
+    private var currentPhotoUri: Uri? = null
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    private val takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+        if (isSuccess) {
+            currentPhotoUri?.let { uri ->
+                photoActionListener.onPhotoCaptured(uri)
+                dismiss()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = LayoutPhotoOptionsBottomSheetBinding.inflate(inflater)
@@ -22,13 +57,87 @@ class PhotoOptionsBottomSheet : BottomSheetDialogFragment() {
         setClickListener()
     }
 
+    private fun init() {
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(parentActivity, "카메라 권한이 허용되었습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(parentActivity, "카메라 권한이 취소되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun setClickListener() = with(binding) {
         binding.clPhotoPick.setOnClickListener {
-            dismiss()
+
         }
 
         binding.clTakePicture.setOnClickListener {
-            dismiss()
+            checkCameraPermission()
+        }
+    }
+
+    private fun checkCameraPermission() {
+        val cameraPermission: String = Manifest.permission.CAMERA
+        val cameraPermissionState = (ContextCompat.checkSelfPermission(
+            parentActivity,
+            Manifest.permission.CAMERA
+        ))
+        when {
+            cameraPermissionState == PackageManager.PERMISSION_GRANTED -> openCamera()
+            shouldShowRequestPermissionRationale(cameraPermission) -> showPermissionRationale()
+            cameraPermissionState == PackageManager.PERMISSION_DENIED -> requestPermissionLauncher.launch(cameraPermission)
+        }
+    }
+
+    private fun showPermissionRationale() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("카메라 권한 요청")
+            .setMessage("카메라 권한을 허용해야 사진 촬영이 가능합니다.")
+            .setPositiveButton("허용") { _, _ ->
+                openAppSettings()
+            }
+            .setNegativeButton("취소", null)
+            .show()
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", requireContext().packageName, null)
+        }
+        startActivity(intent)
+    }
+
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(parentActivity.packageManager) != null) {
+            try {
+                createImageFile().let { photoFile ->
+                    currentPhotoUri = FileProvider.getUriForFile(
+                        parentActivity,
+                        "${parentActivity.packageName}.provider",
+                        photoFile
+                    )
+                }
+                currentPhotoUri?.let { uri ->
+                    takePictureLauncher.launch(uri)
+                }
+            } catch (e: Exception) {
+                Toast.makeText(parentActivity, "카메라 앱을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = parentActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).also {
+            Log.d("!! absolutePath : ", it.absolutePath.toString())
         }
     }
 }

b0d202cf98f7908ba5bef8d3356a7c01f8bf2cf0
ggone212 <stopone3119@neptunez.co.kr>
2024-12-10 13:33:23 +0900

    [feature] Glide Uri 타입 추가

--- app/src/main/java/com/neptune/klat_uikit_android/core/extension/ImageExtension.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/core/extension/ImageExtension.kt
@@ -1,12 +1,21 @@
 package com.neptune.klat_uikit_android.core.extension
 
+import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import java.net.URI
 
 fun ImageView.loadThumbnail(url: String) {
     Glide.with(this)
         .load(url)
+        .transform(CenterCrop())
+        .into(this)
+}
+
+fun ImageView.loadThumbnail(uri: Uri) {
+    Glide.with(this)
+        .load(uri)
         .transform(CenterCrop())
         .into(this)
 }

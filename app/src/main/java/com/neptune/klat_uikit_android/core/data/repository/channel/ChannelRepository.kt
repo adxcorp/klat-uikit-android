ef1c87f1f92f4e44db803a7ce7e4a2b016f77f6b
ggone212 <stopone3119@neptunez.co.kr>
2024-12-12 15:44:28 +0900

    [feature] repository observeChannel 함수 구현

--- app/src/main/java/com/neptune/klat_uikit_android/core/data/repository/channel/ChannelRepository.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/core/data/repository/channel/ChannelRepository.kt
@@ -3,8 +3,12 @@ package com.neptune.klat_uikit_android.core.data.repository.channel
 import com.neptune.klat_uikit_android.core.data.model.base.Result
 import com.neptune.klat_uikit_android.core.data.model.base.WrappedFailResult
 import com.neptune.klat_uikit_android.core.data.model.channel.ChannelListResponse
+import com.neptune.klat_uikit_android.core.data.model.channel.EventType
+import com.neptune.klat_uikit_android.core.data.model.channel.ObserveChannelResponse
 import io.talkplus.TalkPlus
+import io.talkplus.TalkPlus.ChannelListener
 import io.talkplus.entity.channel.TPChannel
+import io.talkplus.entity.channel.TPMessage
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -23,10 +27,47 @@ class ChannelRepository {
                 }
 
                 override fun onFailure(errorCode: Int, exception: Exception) {
-                    trySend(Result.Failure(WrappedFailResult(errorCode = errorCode, exception = exception)))
+                    trySend(Result.Failure(WrappedFailResult(
+                        errorCode = errorCode,
+                        exception = exception)
+                    ))
                 }
             })
             awaitClose { cancel() }
         }
+    }
+
+    fun observeChannel(tag: String): Flow<ObserveChannelResponse> = callbackFlow {
+        TalkPlus.addChannelListener(tag, object : ChannelListener {
+            override fun onMessageReceived(tpChannel: TPChannel, tpMessage: TPMessage) {
+                trySend(ObserveChannelResponse(
+                    type = EventType.RECEIVED_MESSAGE,
+                    channel = tpChannel,
+                    message = tpMessage
+                ))
+            }
+
+            override fun onChannelChanged(tpChannel: TPChannel) {
+                trySend(ObserveChannelResponse(
+                    type = EventType.CHANGED_CHANNEL,
+                    channel = tpChannel,
+                ))
+            }
+
+            override fun onChannelAdded(tpChannel: TPChannel) {
+                trySend(ObserveChannelResponse(
+                    type = EventType.ADDED_CHANNEL,
+                    channel = tpChannel,
+                ))
+            }
+
+            override fun onChannelRemoved(tpChannel: TPChannel) {
+                trySend(ObserveChannelResponse(
+                    type = EventType.REMOVED_CHANNEL,
+                    channel = tpChannel,
+                ))
+            }
+        })
+        awaitClose { cancel() }
     }
 }

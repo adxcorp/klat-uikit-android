9d75b68ccabce2fafdf1c06b9f093a112ea8f4b6
ggone212 <stopone3119@neptunez.co.kr>
2025-01-07 17:11:20 +0900

    [chore] url이 빈 문자열인 경우 로직 추가

--- app/src/main/java/com/neptune/klat_uikit_android/core/extension/ImageExtension.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/core/extension/ImageExtension.kt
@@ -4,9 +4,14 @@ import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import java.net.URI
+import com.neptune.klat_uikit_android.R
 
 fun ImageView.loadThumbnail(url: String) {
+    if (url == "") {
+        loadThumbnail(R.drawable.ic_48_default_logo)
+        return
+    }
+
     Glide.with(this)
         .load(url)
         .transform(CenterCrop())

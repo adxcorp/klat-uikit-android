4c054fea9953ab98a9d95eedde802883cf08d0fa
ggone212 <stopone3119@neptunez.co.kr>
2025-02-12 17:08:31 +0900

    [chore] 버전 카탈로그 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,15 +52,15 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.appcompat)
+//    implementation(libs.material)
+//    implementation(libs.androidx.activity)
+//    implementation(libs.androidx.constraintlayout)
+//
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
 
     api("androidx.core:core-ktx:1.1.0")
     api("androidx.appcompat:appcompat:1.3.0")

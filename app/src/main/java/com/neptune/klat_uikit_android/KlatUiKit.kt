774c19a2406f485bb49a56a7577a5a54441ae32a
ggone212 <stopone3119@neptunez.co.kr>
2025-02-12 10:30:51 +0900

    [feature] 1:1 채팅방 이동 구현

--- app/src/main/java/com/neptune/klat_uikit_android/KlatUiKit.kt
+++ app/src/main/java/com/neptune/klat_uikit_android/KlatUiKit.kt
@@ -3,13 +3,13 @@ package com.neptune.klat_uikit_android
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.widget.Toast
 import com.neptune.klat_uikit_android.core.base.ChannelObject
 import com.neptune.klat_uikit_android.feature.channel.main.ChannelActivity
-import com.neptune.klat_uikit_android.feature.channel.search.ChannelSearchActivity
+import com.neptune.klat_uikit_android.feature.chat.ChatActivity
 import com.neptune.klat_uikit_android.model.LoginRequest
 import io.talkplus.TalkPlus
+import io.talkplus.entity.channel.TPChannel
 import io.talkplus.entity.user.TPUser
 import io.talkplus.params.TPLoginParams
 import java.lang.Exception
@@ -20,7 +20,11 @@ object KlatUiKit {
         ChannelObject.tag = appId
     }
 
-    fun connect(activity: Activity, loginRequest: LoginRequest) {
+    fun connect(
+        activity: Activity,
+        loginRequest: LoginRequest,
+        oneToOneChannelId: String? = null
+    ) {
         val loginType = loginRequest.userToken?.let { TPLoginParams.LoginType.TOKEN } ?: TPLoginParams.LoginType.ANONYMOUS
 
         val tpLoginParams = TPLoginParams.Builder(loginRequest.userId, loginType).apply {
@@ -31,8 +35,29 @@ object KlatUiKit {
 
         TalkPlus.login(tpLoginParams, object : TalkPlus.CallbackListener<TPUser> {
             override fun onSuccess(tpUser: TPUser) {
-                val intent = Intent(activity, ChannelActivity::class.java)
                 ChannelObject.userId = tpUser.userId
+                oneToOneChannelId?.let { channelId ->
+                    getChannel(channelId, activity)
+                } ?: run {
+                    val intent = Intent(activity, ChannelActivity::class.java)
+                    activity.startActivity(intent)
+                }
+            }
+
+            override fun onFailure(errorCode: Int, exception: Exception) {
+                Toast.makeText(activity, exception.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun getChannel(
+        channelId: String,
+        activity: Activity
+    ) {
+        TalkPlus.getChannel(channelId, object : TalkPlus.CallbackListener<TPChannel> {
+            override fun onSuccess(tpChannel: TPChannel) {
+                ChannelObject.setTPChannel(tpChannel)
+                val intent = Intent(activity, ChatActivity::class.java)
                 activity.startActivity(intent)
             }
 
